bb4a7096eba2d83bfa9a1213397baf51da42e4f2
Julius Häger <julius_hager@hotmail.com>
2016-09-17 01:55:56 +0200

    Added a new debug project

---
+++ SKON.NET/VerboseScannerParser/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SKON;
+using SKON.Internal;
+
+namespace VerboseScannerParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string defaultPath = "./SKONTest.skon";
+
+            Console.Write("Input file?:");
+
+            string filePath = Console.ReadLine();
+
+            if (File.Exists(filePath) == false)
+            {
+                filePath = defaultPath;
+            }
+
+            SKONObject obj = VerboseParseFile(filePath);
+
+            Console.WriteLine();
+
+            Console.WriteLine(SKON.SKON.Write(obj));
+
+            Console.ReadKey(true);
+        }
+
+        public static SKONObject VerboseParseFile(string path)
+        {
+            if (File.Exists(path) == false)
+            {
+                Console.WriteLine("Could not find file {0}!", path);
+                return new SKONObject();
+            }
+
+            Console.WriteLine("Found file {0}", path);
+
+            Console.WriteLine("Creating scanner from file...");
+            Scanner sc = new Scanner(path);
+            
+            Console.WriteLine("Created scanner with Buffer type of: {0}!", sc.buffer.GetType());
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            int tokens = 0;
+
+            Token t = new Token();
+            while ((t = sc.Scan()).kind != 0)
+            {
+                Console.WriteLine("Reading a token of type {0} with value \"{1}\".", t.kind, t.val);
+                Console.WriteLine();
+                tokens++;
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("Done tokenizing file! Got {0} tokens!", tokens);
+
+            sc = new Scanner(path);
+
+            Parser parser = new Parser(sc);
+
+            parser.Parse();
+            
+            return parser.data;
+        }
+    }
+}
